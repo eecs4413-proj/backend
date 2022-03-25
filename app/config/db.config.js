0ac2803c0cacc3e24302db18a5b7986aee7ee97c
Jay Chung <wayne961025@gmail.com>
2022-03-25 13:55:38 -0400

    Registration API RetrieveAll/RetrieveOne/Update/Delete/Create

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,18 @@
-module.exports =
-{
-    HOST: "conviencestore.cw3crbgltbxe.us-east-1.rds.amazonaws.com",
-    USER: "admin",
-    PASSWORD: "eecs4413backend",
-    DB: "ConviStore"
-};
+const mysql = require('mysql');
+
+
+
+const dbConn = mysql.createConnection({
+    host: 'conviencestore.cw3crbgltbxe.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'eecs4413backend',
+    database: 'ConviStore'
+});
+
+dbConn.connect(function(error){
+    if(error) throw error;
+    console.log('Database Connected Successfully!');
+})
+
+module.exports = dbConn;
 
--- app/controllers/registration.cont.js
+++ app/controllers/registration.cont.js
@@ -1,38 +1,70 @@
-const User = require("../models/registration.model.js");
 
-exports.create = (req,res) => {
-    if(!req.body){
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    }
-    const user = new User({
-        email: req.body.email,
-        pw: req.body.pw,
-        admin: req.body.admin
-    });
-    User.create(user, (err,data)=>{
+const User = require('../models/registration.model');
+const UserModel = require('../models/registration.model');
+
+//get all user list 
+exports.getUserList = (req,res) =>{
+    //console.log('here all user list');
+    UserModel.getAllUsers((err,users) =>{
+        console.log('We are here');
         if(err)
-            res.status(500).send({
-                message:
-                    err.message || "Error ocurred while creating the User."
-            });
-        else res.send(data);
-    });
-};
+        res.send(err);
+        console.log('Users', users);
+        res.send(users);
 
-exports.findEmail = (req,res) => {
-    User.findByEmail(req.params.email, (err, data) => {
-        if(err) {
-            if(err.kind == "not_found") {
-                res.status(404).send({
-                    message: `Not found User with email ${req.params.email}`
-                });
-            }else{
-                res.status(500).send({
-                    message: "Error retrieving User with id " + req.params.email
-                });
-            }
-        }else res.send(data);
-    });
-};
+    })
+}
+
+//get User by email
+exports.getUserByEmail = (req,res) =>{
+   // console.log('get user by email');
+   UserModel.getUserByEmail(req.params.email, (err,user)=>{
+        if(err)
+        res.send(err);
+        console.log('Single User data',user);
+        res.send(user);
+   })
+}
+
+//create new user 
+exports.createNewUser = (req, res) =>{
+    const userReqData = new User(req.body);
+    console.log('userReqData', userReqData);
+    //check null
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+        res.send(400).send({success: false, message: 'Please fill all fields'});
+    }else{
+        console.log('valid data');
+        UserModel.createUser(userReqData,(err,user)=>{
+            if(err)
+            res.send(err);
+            res.json({status: true, message: 'User Created Successfully',data: user.insertEmail})
+        })
+    }
+}
+
+// update user 
+exports.updateUser = (req,res)=>{
+    const userReqData = new User(req.body);
+    console.log('userReqData update', userReqData);
+    //check null
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+        res.send(400).send({success: false, message: 'Please fill all fields'});
+    }else{
+        console.log('valid data');
+        UserModel.updateUser(req.params.email,userReqData,(err,user)=>{
+            if(err)
+            res.send(err);
+            res.json({status: true, message: 'User updated Successfully'})
+        })
+    }
+}
+
+//delete user 
+exports.deleteUser = (req,res) =>{
+    UserModel.deleteUser(req.param.email, (err,user)=>{
+        if(err)
+        res.send(err);
+        res.json({success:true, message: "User deleted successfully"});
+    })
+}
